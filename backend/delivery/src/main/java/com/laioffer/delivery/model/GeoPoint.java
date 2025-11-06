28d5cfc6356074a002a96666af669e2554b02451
GeliDada <tinnieq@me.com>
2025-11-05 16:31:45 -0800

    Revert "Merge branch 'develop' of https://github.com/DeliveryDevelopment/DeliveryDevelopment into develop"
    This reverts commit fcbe2dc913ac458205035487f280ac986bd180ca, reversing
    changes made to 4be3533aae749a7450cf0342c27a98a079a79bb0.

--- backend/delivery/src/main/java/com/laioffer/delivery/model/GeoPoint.java
+++
@@ -1,8 +0,0 @@
-package com.laioffer.delivery.model;
-
-
-public record GeoPoint(
-        double lat,
-        double lon
-) {
-}
--- backend/delivery/src/main/java/com/laioffer/delivery/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.laioffer.delivery.model;
-
-//only two types can be taken
-public enum UserRole {
-    ROLE_HOST,
-    ROLE_GUEST,
-}
